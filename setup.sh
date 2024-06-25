174b36d3bc4492c77e8322611474cea1cd232ddd
Ailec1 <126810380+Ailec1@users.noreply.github.com>
2024-06-25 22:39:15 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,8 +11,8 @@ while true; do
                     "7" "Install MISP" \
                     "8" "Setup IRIS <-> Wazuh Integration" \
                     "9" "Setup MISP <-> Wazuh Integration" \
-                    "10" "Show Status" 3>&1 1>&2 2>&3) \
-                    "11" "Delete All Containers, Images, Volumes, and Networks"
+                    "10" "Show Status" \
+                    "11" "Delete All Containers, Images, Volumes, and Networks" 3>&1 1>&2 2>&3)
     # Script version 1.0 updated 15 November 2023
     # Depending on the chosen option, execute the corresponding command
     case $OPTION in
